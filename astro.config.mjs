c2c46da0785ca82a1879aa1923516aa791254d03
marcwagn <39244727+marcwagn@users.noreply.github.com>
2024-08-18 20:36:46 +0200

    fix config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://marcwagn.github.io",
-  base: "/mcw_blog ",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
